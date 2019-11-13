93d951c0df9e2d3552c6338dc3669d5b04ec366d
April Matus <apmartus8@gmail.com>
2019-11-13 10:54:31 -0700

    Some code

--- roygbiv.js
+++ roygbiv.js
@@ -2,5 +2,12 @@ function ready() {
     console.log("page ready");
 
 }
+document.body.style.background = "red";
+
+setInterval(() => {
+    document.body.style.background = "orange";
+
+}, 2000)
+
 
 document.addEvent addEventListener("DOMContentLoaded", ready);
