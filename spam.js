38b8da815b1413561d9cbdbb054bcc103e79c2c8
April Matus <apmartus8@gmail.com>
2019-11-15 10:24:30 -0700

    Some Progress

--- spam.js
+++ spam.js
@@ -1,11 +1,15 @@
-function ready() {
-    console.log("page ready");
+// function ready() {
+//     console.log("page ready");
 
-    hazzah.append("hazzah");
+//     hazzah.append("hazzah");
 
     // hazzah.onclick = function() {
     //     hazzah.append("Hazzah");
     // };
-}
+// }
+
+function myFunction() {
+  document.getElementById("hazzah").innerHTML = "Hazzah";
+};
 
 document.addEvent addEventListener("DOMContentLoaded", ready);
