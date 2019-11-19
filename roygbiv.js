2487786e13b2d7cef0dd62b6f1865b60ff886545
April Matus <apmartus8@gmail.com>
2019-11-19 10:21:45 -0700

    Code for card should work but isnt

--- roygbiv.js
+++ roygbiv.js
@@ -1,13 +1,9 @@
-function ready() {
-    console.log("page ready");
+for(i=0; i<5; i) {
+    setInterval(() => {
+        document.body.style.background = "orange";
+    }, 2000)
 
 }
-document.body.style.background = "red";
-
-setInterval(() => {
-    document.body.style.background = "orange";
-
-}, 2000)
 
 
 document.addEventListener("DOMContentLoaded", ready);
